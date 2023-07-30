a1fbabe17cb5c478231d0cf35d7fc2b5fa2ac63c
Abhishek Panda <abxisxekpanda@gmail.com>
2023-07-31 02:37:21 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Birb extension
+A simple extension that replaces the ugly X logo with the modern twitter bird logo. Also fixes and replace the "/X" with "/Twitter" in the tab title.
+
+## To-do:
+- [x] Replace the X with blue bird on top left corner
+- [x] Replace the "/X" with "/Twitter"
+- [ ] Make an exception for "Lights Out" theme and add the black and white bird instead of the blue bird (HELP HOW AM I SUPPOSED TO FIND THAT OUT 😭💀)
+
+## Optionals:-
+- [ ] Custom "CSS" injection
+- [ ] Custom "Logo" injection
---
+++ scripts/xiconfix.js
@@ -0,0 +1,51 @@
+const logoObserver = new MutationObserver((mutations) => {
+  mutations.forEach((mutation) => {
+    if (mutation.type === "childList") {
+      logoFix();
+      faviconFix();
+    }
+  });
+});
+
+const titleObserver = new MutationObserver((mutations) => {
+  mutations.forEach((mutation) => {
+    if (mutation.type === "childList") {
+      titleFix();
+    }
+  });
+});
+
+function faviconFix() {
+  document.querySelector('link[rel="shortcut icon"]').href =
+    chrome.runtime.getURL("../assets/favicon.png");
+}
+function logoFix() {
+  const twitterLogoSelector = 'h1[role="heading"] a[aria-label="Twitter"]';
+  const element = document.querySelector(twitterLogoSelector);
+  if (!element) {
+    console.log("Logo not detected");
+    return;
+  } else {
+    const newLogo = document.createElement("img");
+    newLogo.src = chrome.runtime.getURL("../assets/twitter.svg");
+    newLogo.alt = "Custom Logo";
+    newLogo.style.cssText = "width: 45px; padding-top: 12px;";
+    element.innerHTML = "";
+    element.appendChild(newLogo);
+    logoObserver.disconnect();
+  }
+}
+
+function titleFix() {
+  if (document.title.endsWith("X")) {
+    document.title = document.title.slice(0, -1) + "Twitter";
+  }
+}
+
+logoObserver.observe(document.body, {
+  childList: true,
+  subtree: true,
+});
+titleObserver.observe(document.querySelector("head"), {
+  childList: true,
+});
