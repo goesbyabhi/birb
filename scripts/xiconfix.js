a74e7af7f8569b4091673eb4361c1194b57ed0d7
Abhishek Panda <abxisxekpanda@gmail.com>
2023-11-17 13:35:46 +0530

    Feat: Theme specific logo

--- scripts/xiconfix.js
+++ scripts/xiconfix.js
@@ -16,6 +16,36 @@ const titleObserver = new MutationObserver((mutations) => {
   });
 });
 
+function getCurrTheme() {
+  const body = document.body;
+  const backgroundColor =
+    body.style.backgroundColor || getComputedStyle(body).backgroundColor;
+
+  if (backgroundColor.startsWith("#")) {
+    const hex = backgroundColor.substring(1);
+    backgroundColor = `rgb(${parseInt(hex.slice(0, 2), 16)}, ${parseInt(
+      hex.slice(2, 4),
+      16,
+    )}, ${parseInt(hex.slice(4, 6), 16)})`;
+  }
+
+  if (backgroundColor === "rgb(0, 0, 0)" || backgroundColor === "#000000") {
+    return "dark";
+  } else if (
+    backgroundColor === "rgb(21, 32, 43)" ||
+    backgroundColor === "#15202B"
+  ) {
+    return "blue";
+  } else if (
+    backgroundColor === "rgb(255, 255, 255)" ||
+    backgroundColor === "#FFFFFF"
+  ) {
+    return "light";
+  } else {
+    return "unknown";
+  }
+}
+
 function faviconFix() {
   document.querySelector('link[rel="shortcut icon"]').href =
     chrome.runtime.getURL("../assets/favicon.png");
@@ -23,14 +53,32 @@ function faviconFix() {
 function logoFix() {
   const twitterLogoSelector = 'h1[role="heading"] a[aria-label="X"]';
   const element = document.querySelector(twitterLogoSelector);
+
   if (!element) {
     console.log("Lmao Logo not detected");
     return;
   } else {
+    const theme = getCurrTheme();
     const newLogo = document.createElement("img");
-    newLogo.src = chrome.runtime.getURL("../assets/twitter.svg");
+
+    switch (theme) {
+      case "dark":
+        newLogo.src = chrome.runtime.getURL("../assets/twitterDark.svg");
+        break;
+      case "blue":
+        newLogo.src = chrome.runtime.getURL("../assets/twitter.svg");
+        break;
+      case "light":
+        newLogo.src = chrome.runtime.getURL("../assets/twitter.svg");
+        break;
+      default:
+        console.log("Unknown theme");
+        return;
+    }
+
     newLogo.alt = "Custom Logo";
-    newLogo.style.cssText = "width: 45px; padding-top: 12px;";
+    newLogo.style.cssText =
+      "width: 30px; height: 30px; margin-top: 10px; margin-right: 5px; padding-left: 8px";
     element.innerHTML = "";
     element.appendChild(newLogo);
     logoObserver.disconnect();
